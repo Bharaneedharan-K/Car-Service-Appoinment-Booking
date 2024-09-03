f996dcd2dd8caab9fca8eeaa46fbe2ce1692386f
Bharaneedharan-K <bharaneedharan2004@gmail.com>
2024-09-03 20:12:20 +0530

    vendor page update

--- vendor/forgot_password.php
+++ vendor/forgot_password.php
@@ -5,8 +5,8 @@ $username = "root"; // Replace with your database username
 $password = ""; // Replace with your database password
 $dbname = "carservice";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
 
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 
 if ($conn->connect_error) {
