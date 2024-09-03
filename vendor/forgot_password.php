6b7445348e7752f5b3de1386e9b80e53d190bd87
Bharaneedharan-K <bharaneedharan2004@gmail.com>
2024-09-03 20:10:38 +0530

    vendor

--- vendor/forgot_password.php
+++ vendor/forgot_password.php
@@ -7,6 +7,8 @@ $dbname = "carservice";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
